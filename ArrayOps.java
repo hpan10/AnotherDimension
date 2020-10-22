59ee30560be437e842fbd4c4b28085cfae902b50
Harry Pan <hpan10@stuy.edu>
2020-10-22 13:20:48 -0400

    4b completed

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,9 +1,19 @@
 public class ArrayOps{
+
   public static int sum(int[] arr){
     int sum = 0;
     for (int start = 0; start < arr.length; start++){
       sum += arr[start];
     }
     return sum;
+  }
+
+
+  public static int largest(int[] arr){
+    int largest = arr[0];
+    for (int start = 0; start < arr.length; start++){
+      if (arr[start] > largest) largest = arr[start];
+    }
+    return largest;
   }
 }
--- Tester.java
+++ Tester.java
@@ -1,6 +1,7 @@
 public class Tester{
   public static void main(String[]args){
-    int[] a = {2,5,6};
+    int[] a = {2,5,6,2,43,2,12};
     System.out.println(ArrayOps.sum(a));
+    System.out.println(ArrayOps.largest(a));
   }
 }
